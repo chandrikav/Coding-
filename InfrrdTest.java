20146e26c5e02297df9e09aaeed9c1c0364234d3
Chandrika <venkob.chandrika@gmail.com>
2020-09-20 16:46:52 +0530

    first commit

---
+++ InfrrdTest.java
@@ -0,0 +1,28 @@
+package chan.code.samples;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InfrrdTest {
+
+	@Test
+	void testException() {
+	    Throwable exception = assertThrows(IllegalArgumentException.class, () -> InfrrdAssignment.pillowMatch(0, 0));
+	    assertEquals("invalid inputs", exception.getMessage());
+		
+	}
+
+	
+	@Test
+	void testbasic() {
+	    assertEquals(9, InfrrdAssignment.pillowMatch(11, 4));
+		
+	}
+	
+	@Test
+	void testbasic2() {
+	    assertEquals(3, InfrrdAssignment.pillowMatch(5, 2));
+		
+	}
+}
